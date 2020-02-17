6b3bd7ee7d49173bdc8263f7459a13d395279e68
APstarwars <32665017+APstarwars@users.noreply.github.com>
2020-02-16 20:36:47 -0800

    Made him spin :3

--- index.js
+++ index.js
@@ -3,24 +3,36 @@ function setPos(x, y) {
     document.getElementById("coon").style.left = x + "px";
 }
 
+function setRotation(angle) {
+    document.getElementById("coon").style.transform = "rotate(" + angle + "deg)";
+}
+
 function update() {
     var x = 0;
     var right = true;
     var y = 0;
     var down = true;
+    var angle = 0;
+    var rotate = 0;
+
     var id = setInterval(frame, 5);
     function frame() {
         // Sets Direction
         if (x >= 1720) {
             right = false;
+            rotate++;
         } else if (x <= 0) {
             right = true;
+            rotate++;
         }
         if (y >= 880) {
             down = false;
+            rotate++;
         } else if (y <= 0) {
             down = true;
+            rotate++;
         }
+
         // Updates position
         if (right) {
             x++;
@@ -33,6 +45,14 @@ function update() {
             y--;
         }
         setPos(x, y);
+
+        // Updates rotation
+        if (rotate % 2 == 0) {
+            angle++;
+        } else {
+            angle--;
+        }
+        setRotation(angle);
     }
 }
 
